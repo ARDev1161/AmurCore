39b70bd3ebf0b70e4c62106fea02454ccac6d229
ARDev1161 <radtarasov@gmail.com>
2020-04-29 04:46:27 +0300

    Add files via upload

---
+++ logic/speech/speechdialog.h
@@ -0,0 +1,55 @@
+#ifndef SPEECHDIALOG_H
+#define SPEECHDIALOG_H
+
+#include <QDialog>
+#include <QTextToSpeech>
+#include <QLoggingCategory>
+
+//#include "sphinxrecognizer.h"
+
+namespace Ui {
+class SpeechDialog;
+}
+
+class SpeechDialog : public QDialog
+{
+    Q_OBJECT
+
+    Ui::SpeechDialog *ui;
+
+    int* rate;
+    int* pitch;
+    int* volume;
+    bool* started;
+
+    QString engineName;
+    QTextToSpeech * speech;
+    QVector<QVoice> * voices;
+    QVector<QString> * engines;
+    QVector<QString> * listRows;
+    QVector<QVariant> * languages;
+
+public:
+    explicit SpeechDialog(QWidget *parent = nullptr);
+    ~SpeechDialog();
+
+public slots:
+    void speak();
+    void stop();
+
+    void setRate(int);
+    void setPitch(int);
+    void setVolume(int volume);
+
+    void stateChanged(QTextToSpeech::State state);
+    void engineSelected(int index);
+    void languageSelected(int language);
+    void voiceSelected(int index);
+
+    void localeChanged(const QLocale &locale);
+
+private slots:
+    void on_buttonBox_accepted();
+};
+
+#endif // SPEECHDIALOG_H
---
+++ logic/speech/sphinxrecognizer.h
@@ -0,0 +1,13 @@
+#ifndef SPHINXRECOGNIZER_H
+#define SPHINXRECOGNIZER_H
+
+//#include "prim_type.h"
+//#include <pocketsphinx.h>
+
+class SphinxRecognizer
+{
+public:
+    SphinxRecognizer();
+};
+
+#endif // SPHINXRECOGNIZER_H
