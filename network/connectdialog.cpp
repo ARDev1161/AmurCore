06007e55dce19561c79153e14764246813017216
ARDev1161 <radtarasov@gmail.com>
2020-04-29 04:50:17 +0300

    Add files via upload

---
+++ network/connectdialog.cpp
@@ -0,0 +1,19 @@
+#include "connectdialog.h"
+#include "ui_connectdialog.h"
+
+ConnectDialog::ConnectDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ConnectDialog)
+{
+    ui->setupUi(this);
+}
+
+ConnectDialog::~ConnectDialog()
+{
+    delete ui;
+}
+
+void ConnectDialog::on_buttonBox_accepted()
+{
+
+}
